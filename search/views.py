653b7934aa199b2ce18c744c4ba4030f438e7b8c
= <asjohnston@alaska.edu>
2016-06-15 20:37:36 +0000

    add xml output

--- search/views.py
+++ search/views.py
@@ -9,19 +9,24 @@ def granule(request):
     cmr_parms = build_cmr_parms(request.GET)
     xml_response = send_cmr_request(cmr_parms)
     results = parse_xml_response(xml_response)
-    if request.GET.get('output', '').upper() == 'CSV':
+
+    output = request.GET.get('output', '').upper()
+
+    if output == 'CSV':
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="results.csv"'
         t = loader.get_template('search/csv.template')
         c = Context({'data': results})
         response.write(t.render(c))
         return response
-    elif request.GET.get('output', '').upper() == 'METALINK':
+    elif output == 'METALINK':
         response = HttpResponse(content_type='text/xml')
         response['Content-Disposition'] = 'attachment; filename="results.metalink"'
         t = loader.get_template('search/metalink.template')
         c = Context({'data': results})
         response.write(t.render(c))
         return response
+    elif output == 'XML':
+       return HttpResponse(xml_response, content_type='text/xml')
     return JsonResponse(results, safe=False)
 
